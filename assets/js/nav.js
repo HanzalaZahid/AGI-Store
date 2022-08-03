a318575f2081e3fd87b9aeae9167139c06a82ead
Hanzala Zahid <61132429+HanzalaZahid@users.noreply.github.com>
2022-08-04 01:23:02 +0500

    COLLECTIONS=>REVISION 1

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,6 +1,7 @@
 $(document).ready(
     ()=>
     {
+        // SUB_MANU TOGGLE
         let dropdownbuttons = document.querySelectorAll('.courses_and_nav .section_wrapper .catagories .nav_list .list > li > a i');
         dropdownbuttons.forEach(element => {
             element.addEventListener('click', (evt)=>
@@ -18,6 +19,42 @@ $(document).ready(
                     element.classList.add('rotate')
                     console.log(dropdownList.classList)
                 }
+            })
+        });
+
+        // NAV HIDE AND DISPLAY USING CLASS ADD
+        let navOpenButton = document.querySelector('.courses_and_nav .section_wrapper .head .options .button_menu');
+        let navCloseButton = document.querySelector('.courses_and_nav .section_wrapper .head > i');
+        let nav = document.querySelector('.courses_and_nav .section_wrapper .catagories');
+
+        navOpenButton.addEventListener('click', ()=>
+        {
+            nav.classList.add('nav_small_screen');
+        })
+        navCloseButton.addEventListener('click', ()=>
+        {
+            nav.classList.remove('nav_small_screen');
+        })
+
+        // MENU ACTIVE TABS
+
+        let listItems = document.querySelectorAll('.catagories .nav_list li');
+        listItems.forEach(element => {
+            element.addEventListener('click', ()=>
+            {
+                listItems.forEach(elem=>
+                    {
+                        if (elem.classList.contains('active'))
+                        {
+                            elem.classList.remove('active');
+                        }
+                        if (elem.classList.contains('current_item'))
+                        {
+                            elem.classList.remove('current_item');
+                        }
+                    })
+                element.classList.add('active');
+                element.classList.add('current_item');
             })
         });
     }
